314140cb408ffa437e1fd7772ccead14d00fde9c
leonzhang77 <zhangjianfeng73@hotmail.com>
2015-12-21 10:09:49 +0800

    add Untracked Files- 12/21 OF

---
+++ MODLibrary/Wicresoft.MODLibrary/Wicresoft.MODLibrarySystem/Wicresoft.MODLibrarySystem.Web/Scripts/UserManageJS/AddUser.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+    AddUserInit();
+});
+
+
+function AddUserInit() {
+    AddUserBind();
+
+    AddUserSubmitBind();
+}
+
+function AddUserBind() {
+    $("#DisplayName").bind("change", function () { multipleValidateField("DisplayName", "VN,VL", 6) });
+
+    $("#RealName").bind("change", function () { validateField("RealName", "VN") });
+
+    $("#LoginName").bind("change", function () { validateField("LoginName", "VN") });
+
+    $("#Password").bind("change", function () { validateField("Password", "VN") });
+
+    $("#Email").bind("change", function () { multipleValidateField("Email", "VN,VE") });
+
+    $("#Late_point").bind("change", function () { multipleValidateField("Late_point", "VN,VNM") });
+
+    $("#TestDateTime").bind("change", function () { validateField("TestDateTime", "VN") });
+    $("#TestDateTime").datepicker({ dateFormat: browserDateFormat, altField: "#TestDateTime_errorData", altFormat: serverDateFormat, changeYear: true, changeMonth: true });
+
+    $("#TestDropDownList").bind("change", function () { validateField("TestDropDownList", "VN") });
+}
+
+function AddUserSubmitBind() {
+    $(this).bind("submit", function () { return AddUserSubmitValidation() });
+}
+
+function AddUserSubmitValidation() {
+    var step1, step2, step3, step4, step5, step6, step7, step8;
+
+    step1 = multipleValidateField("DisplayName", "VN,VL", 6);
+    step2 = validateField("RealName", "VN");
+    step3 = validateField("LoginName", "VN");
+    step4 = validateField("Password", "VN");
+    step5 = multipleValidateField("Email", "VN,VE");
+    step6 = multipleValidateField("Late_point", "VN,VNM");
+    step7 = validateField("TestDateTime", "VN");
+    step8 = validateField("TestDropDownList", "VN");
+
+    if (step1 && step2 && step3 && step4 && step5 && step6 && step7 && step8) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
