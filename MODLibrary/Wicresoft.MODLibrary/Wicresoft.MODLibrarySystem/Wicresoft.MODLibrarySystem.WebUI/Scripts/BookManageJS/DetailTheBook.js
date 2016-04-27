6b32a663e29cc9b68d4ff932aa44be0a6c487236
Leonzh <zhangjianfeng73@hotmail.com>
2016-04-27 17:39:35 +0800

    SupportsAndObjections 3

---
+++ MODLibrary/Wicresoft.MODLibrary/Wicresoft.MODLibrarySystem/Wicresoft.MODLibrarySystem.WebUI/Scripts/BookManageJS/DetailTheBook.js
@@ -0,0 +1,10 @@
+﻿
+function support(i) {
+    var supportVal = document.getElementById("Supports");
+    supportVal.innerText = supportVal.innerText/1 + 1;
+}
+
+function objection() {
+    var objectionVal = document.getElementById("Objections");
+    objectionVal.innerText = objectionVal.innerText / 1 - 1;
+}
