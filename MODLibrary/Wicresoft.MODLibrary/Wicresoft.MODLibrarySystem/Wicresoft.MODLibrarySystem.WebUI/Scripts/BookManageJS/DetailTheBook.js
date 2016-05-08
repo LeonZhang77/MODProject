0b283f2ea07162fdac4545e7899f7ef359237e72
leonzhang77 <zhangjianfeng73@hotmail.com>
2016-05-08 23:06:30 +0800

    update dialog for supports and objections

--- MODLibrary/Wicresoft.MODLibrary/Wicresoft.MODLibrarySystem/Wicresoft.MODLibrarySystem.WebUI/Scripts/BookManageJS/DetailTheBook.js
+++ MODLibrary/Wicresoft.MODLibrary/Wicresoft.MODLibrarySystem/Wicresoft.MODLibrarySystem.WebUI/Scripts/BookManageJS/DetailTheBook.js
@@ -1,10 +1,43 @@
-﻿
-function support(i) {
-    var supportVal = document.getElementById("Supports");
-    supportVal.innerText = supportVal.innerText/1 + 1;
+﻿$(function () {
+    $("#supportOrObjection-dialog-confirm").dialog(
+    {
+        autoOpen: false,
+        resizable: false,
+        height: 200,
+        width: 500,
+        modal: true,
+        buttons: {
+            "Yes": function () {
+                $(this).dialog('close');
+                doSOOWork();
+            },
+            "Cancel": function () {
+                $(this).dialog('close');
+            }
+        }
+    })
+});
+
+var _selectItem;
+var _doWorkType;
+var _id;
+
+var bindSOOConfirmDialog = function (obj, i, dSOOWType) {
+    $("#supportOrObjection-dialog-confirm").dialog("open");
+    _selectItem = $(obj);
+    _id = i;
+    _doSOOWorkType = dSOOWType;
 }
 
-function objection() {
-    var objectionVal = document.getElementById("Objections");
-    objectionVal.innerText = objectionVal.innerText / 1 - 1;
+function doSOOWork() {
+    if (_doSOOWorkType == "supports") {
+        var supportVal = document.getElementById("Supports");
+        supportVal.innerText = supportVal.innerText / 1 + 1;
+    }
+    else {
+        var objectionVal = document.getElementById("Objections");
+        objectionVal.innerText = objectionVal.innerText / 1 - 1;
+    }
+    $("#SupportsActionLink").hide();
+    $("#ObjectionsActionLink").hide();
 }
