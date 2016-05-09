4a08a6a0504aaebc81440081b55cf439226e9bac
leonzhang77 <zhangjianfeng73@hotmail.com>
2016-05-09 15:23:03 +0800

    intial of Chart.

---
+++ MODLibrary/Wicresoft.MODLibrary/Wicresoft.MODLibrarySystem/Wicresoft.MODLibrarySystem.Web/Controllers/ChartController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wicresoft.MODLibrarySystem.Web.Models;
+
+namespace Wicresoft.MODLibrarySystem.Web.Controllers
+{
+    public class ChartController : Controller
+    {
+
+        public ActionResult Demo()
+        {
+            return View();
+        }
+        public JsonResult GetDemoLineChartJson()
+        {
+            _getLineChartModelMethod = () =>
+            {
+                return new LabelAndDataForChart()
+                {
+                    XLabels = new string[] { "Lip", "Chang" },
+                    YData = new int[] { 0, 100 }
+                };
+            };
+            return Json(_getLineChartModelMethod(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetDemoPieChartJson()
+        {
+            _getPieChartModelMethod = () =>
+            {
+                return new DataAndColorForChart[]
+                {
+                    new DataAndColorForChart()
+                    {
+                        value=50,
+                        color="#F38630",
+                        highlight="#616774",
+                        label="Chang"
+                    },
+                    new DataAndColorForChart()
+                    {
+                        value =50,
+                        color="#E0E4CC",
+                        highlight="#E0E4CC",
+                        label="Lip"
+                    }
+                };
+            };
+            return Json(_getPieChartModelMethod(), JsonRequestBehavior.AllowGet);
+        }
+
+        #region Base
+        /// <summary>
+        /// Delegate of get line chart model method.
+        /// </summary>
+        Func<LabelAndDataForChart> _getLineChartModelMethod;
+
+        /// <summary>
+        /// Delegate of get pie chart model method.
+        /// </summary>
+        Func<DataAndColorForChart[]> _getPieChartModelMethod;
+
+
+
+
+        #endregion
+    }
+}
---
+++ MODLibrary/Wicresoft.MODLibrary/Wicresoft.MODLibrarySystem/Wicresoft.MODLibrarySystem.Web/Models/ChartModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wicresoft.MODLibrarySystem.Web.Models
+{
+    /// <summary>
+    /// Chart data model for those types:
+    /// Polar area
+    /// Pie
+    /// Doughnut
+    /// </summary>
+    public class DataAndColorForChart
+    {
+        public int value { get; set; }
+        public string color { get; set; }
+        public string highlight { get; set; }
+        public string label { get; set; }
+    }
+
+    /// <summary>
+    /// Chart data model for those types:
+    /// Line
+    /// Bar
+    /// Radar
+    /// </summary>
+    public class LabelAndDataForChart
+    {
+        public string[] XLabels { get; set; }
+        public int[] YData { get; set; }
+        public int[] YData2 { get; set; }
+    }
+}
