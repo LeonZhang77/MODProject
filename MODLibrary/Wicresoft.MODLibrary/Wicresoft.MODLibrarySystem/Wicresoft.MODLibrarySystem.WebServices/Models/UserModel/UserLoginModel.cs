28c2cfdcd16b5f5334b276a733da7311ff9cc1c8
Leonzh <zhangjianfeng73@hotmail.com>
2016-07-07 11:29:04 +0800

    restore WebServices

---
+++ MODLibrary/Wicresoft.MODLibrary/Wicresoft.MODLibrarySystem/Wicresoft.MODLibrarySystem.WebServices/Models/UserModel/UserLoginModel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Wicresoft.MODLibrarySystem.Entity;
+using Wicresoft.MODLibrarySystem.Unity.Helper;
+
+namespace Wicresoft.MODLibrarySystem.WebServices.Models.UserModel
+{
+    public class UserLoginModel : BaseModel
+    {
+        public String DisplayName
+        {
+            get;
+            set;
+        }
+
+        public String RealName
+        {
+            get;
+            set;
+        }
+
+        public String LoginName
+        {
+            get;
+            set;
+        }
+
+        public String Password
+        {
+            get;
+            set;
+        }
+        public int Floor
+        {
+            get;
+            set;
+        }
+        public String PM
+        {
+            get;
+            set;
+        }
+
+        public String Team
+        {
+            get;
+            set;
+        }
+
+        public String Chinese_Name
+        {
+            get;
+            set;
+        }
+        
+        public String Email
+        {
+            get;
+            set;
+        }
+
+        public String Wechat
+        {
+            get;
+            set;
+        }
+        public String Grade
+        {
+            get;
+            set;
+        }
+
+        public String Late_point
+        {
+            get;
+            set;
+        }
+
+        public String Remark
+        {
+            get;
+            set;
+        }
+
+        public String statusMessage
+        {
+            get;
+            set;
+        }
+
+        public static UserLoginModel GetServerModel(UserInfo entity)
+        {
+            UserLoginModel model = new UserLoginModel();
+
+            model.ID = entity.ID;
+            model.DisplayName = entity.DisplayName;
+            model.RealName = entity.RealName;
+            model.LoginName = entity.LoginName;
+            model.Password = entity.Password;
+            model.Floor = entity.Floor;
+            model.Team = entity.Team;
+            model.Chinese_Name = entity.Chinese_Name;
+            model.Email = entity.Email;
+            model.Wechat = entity.Wechat;
+            model.Late_point = entity.Late_point.ToString();
+            model.Remark = entity.Remark;
+            model.Grade = EnumHelper.GetEnumDescription(entity.Grade);
+
+            model.CreateTime = entity.CreateTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+            return model;
+        }        
+    }
+}
---
+++ MODLibrary/Wicresoft.MODLibrary/Wicresoft.MODLibrarySystem/Wicresoft.MODLibrarySystem.WebServices/Models/UserModel/UserServerModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Wicresoft.MODLibrarySystem.Entity;
+using Wicresoft.MODLibrarySystem.Unity.Helper;
+
+namespace Wicresoft.MODLibrarySystem.WebServices.Models.UserModel
+{
+    public class UserServerModel : BaseModel
+    {
+        public String DisplayName
+        {
+            get;
+            set;
+        }
+
+        public String Grade
+        {
+            get;
+            set;
+        }
+
+        public String Password
+        {
+            get;
+            set;
+        }
+
+        public String Email
+        {
+            get;
+            set;
+        }
+
+        public String statusMessage
+        {
+            get;
+            set;
+        }
+
+        public static UserServerModel GetServerModel(UserInfo entity)
+        {
+            UserServerModel model = new UserServerModel();
+
+            model.ID = entity.ID;
+            model.DisplayName = entity.DisplayName;
+            model.Grade = EnumHelper.GetEnumDescription(entity.Grade);
+            model.Email = entity.Email;
+            model.Password = entity.Password;
+            model.CreateTime = entity.CreateTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+            return model;
+        }
+
+        public UserInfo GetEntity(String DisplayName, String RealName, String LoginName, String Password, String Email, String Wechat)
+        {
+            UserInfo user = new UserInfo();
+            user.DisplayName = DisplayName;
+            user.RealName = RealName;
+            user.LoginName = LoginName;
+            user.Password = Password;
+            user.Email = Email;
+            user.Wechat = Wechat;
+
+            return user;
+        }
+
+        public UserInfo UpdateEntity(UserInfo user, String Username, String Password, String Email)
+        {
+            user.DisplayName = Username;
+            user.Password = Password;
+            user.Email = Email;
+            return user;
+        }
+    }
+}
