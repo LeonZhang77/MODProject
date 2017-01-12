d4336a4e34ca38b64c3b9f9196acedcc309b6edb
leonzhang77 <zhangjianfeng73@hotmail.com>
2017-01-12 21:17:52 +0800

    finished some function of MatchInput.

--- MODLibrary/Wicresoft.Badminton/Badminton/Scripts/MatchInput/MatchInputJS.js
+++ MODLibrary/Wicresoft.Badminton/Badminton/Scripts/MatchInput/MatchInputJS.js
@@ -1,4 +1,28 @@
-﻿function setCompetingType()
+﻿$(function(){
+    $("#MatchDate").datepicker();
+    $("#inputFeild").hide();
+
+    $("#AddMatchDialog").dialog(
+    {
+        autoOpen: false,
+        resizable: false,
+        height: 400,
+        width: 500,
+        modal: true,
+        buttons: {
+            "OK": function () {
+                $(this).dialog('close');
+                doAddMatch();
+            },
+            "Cancel": function () {
+                $(this).dialog('close');
+            }
+        }
+    });
+
+});
+
+function setCompetingType()
 {
     var selectedChampionshipID = $("#SelectChampionship option:selected").val();
     
@@ -12,7 +36,6 @@
         }
     }
 
-    alert(CompetingType);
     dropdownList = document.getElementById("SelectCompetingType");    
     for (var index = 0; index < dropdownList.options.length; index++) {
         if (dropdownList.options[index].text == CompetingType) {
@@ -20,6 +43,33 @@
             break;
         }
     }
-    alert(CompetingType);
+   
     $("#CompetingType").val(CompetingType);
+   // if (CompetingType == "" || CompetingType == "" || CompetingType == "" || CompetingType.contains("Singles"))
+    if (CompetingType.indexOf("Singles")!=-1)
+    {
+        $("#Winner2TR").hide();
+        $("#Loser2TR").hide();
+    }
+
+    if (CompetingType.indexOf("Male's") != -1)
+    {
+      
+    }
+
+    if (CompetingType.indexOf("Female's") != -1)
+    {
+        $("Winner1List").empty();
+
+
+    }
+
+    $("#inputFeild").show();
+}
+
+function doAddMatch() { }
+
+function addMatch()
+{
+    $("#AddMatchDialog").dialog("open");
 }
